1da45da255c73305cda9f41f266d6be7945b8c62
Valdevina <valdevina.digita@gmail.com>
2025-03-11 21:43:46 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+/* 
+<h2>Cardapio do Dia</h2>
+<h3>Segunda-feira</h3>
+<section>
+        <ul>
+        <li>Arroz</li>
+        <li>Feijão</li>
+        <li>Salada</li>
+        </ul>
+
+
+        <figure>
+        <img src="img/image (1).png" alt="feijoada">
+        </figure>
+</section>
+*/
